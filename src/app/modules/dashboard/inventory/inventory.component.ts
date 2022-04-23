f7d76bffae258727a53ee02a0baf930a02b662e7
Ivy Ti <chivy1221@gmail.com>
2022-04-23 05:15:03 +0300

    inventory update

--- src/app/modules/dashboard/inventory/inventory.component.ts
+++ src/app/modules/dashboard/inventory/inventory.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
 })
 export class InventoryComponent implements OnInit {
+  art_grid = [1, 2, 3, 4];
+  collections = [1, 2];
+  view_mode: 'collection' | 'products' = 'collection';
+  product_list = [1, 2, 3, 4, 5];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
